94a70a45f1d8718e6fac40d816ec1344f65f0ca8
Skatler <yaroslavzozuliya95@gmail.com>
2023-01-31 11:40:38 +0200

    v4

--- js/console_application.js
+++ js/console_application.js
@@ -1,4 +1,4 @@
-const nuberOfFilms = +prompt('Сколько фильмов вы уже посмотрели', '');
+const nuberOfFilms = prompt('Сколько фильмов вы уже посмотрели', '');
 
 const personalMovieDB = {
   count: nuberOfFilms,
@@ -10,26 +10,21 @@ const personalMovieDB = {
 
 for (let i = 0; i < 2; i += 1) {
   const a = prompt('Один из последних просмотренный фильмов ?', '');
-  const b = prompt('Сколько фильмов вы уже посмотрели ?');
+  const b = prompt('На сколько оцените его ?');
 
-  if (a != null && b != null && a !== '' && b !== '' && a.length < 50) {
+  if (a !== null && b !== null && a !== '' && b !== '' && a.length < 50) {
     personalMovieDB.movies[a] = b;
-
     console.log('done');
   } else {
     console.log('error');
-    i -= 1;
+    i += 1;
   }
 }
-
 if (personalMovieDB.count < 10) {
-  console.log('Просмотренно довольно мало фильмов');
+  console.log('Вы посмотрели довольно мало фильмов');
 } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
   console.log('Вы классический зритель');
-} else if (personalMovieDB.count >= 30) {
+} else if (personalMovieDB >= 30) {
   console.log('Вы киноман');
-} else {
-  console.log('Произошла ошибка');
 }
-
 console.log(personalMovieDB);
